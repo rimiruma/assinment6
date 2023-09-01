9fd4cbc0ced7bee75e574b0b6073636205db1835
Rimi Ruma <rimiruma12@gmail.com>
2023-09-01 20:56:42 +0600

    done

--- script.js
+++ script.js
@@ -1,65 +1,14 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Fetch the categories data
-//     fetch("https://openapi.programming-hero.com/api/videos/categories")
-//         .then(response => response.json())
-//         .then(data => {
-//             const categoriesContainer = document.querySelector(".categories");
-
-//             // Create and populate the category buttons
-//             data.data.forEach(category => {
-//                 const categoryButton = document.createElement("button");
-//                 categoryButton.textContent = category.category;
-//                 categoryButton.addEventListener("click", () => {
-//                     fetchVideosByCategory(category.category_id);
-//                 });
-//                 categoriesContainer.appendChild(categoryButton);
-//             });
-//         })
-//         .catch(error => {
-//             console.error("Error fetching categories:", error);
-//         });
-
-//     // Function to fetch and display videos by category
-//     function fetchVideosByCategory(categoryId) {
-//         fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
-//             .then(response => response.json())
-//             .then(data => {
-//                 const videosContainer = document.querySelector(".videos");
-//                 videosContainer.innerHTML = ""; // Clear existing video cards
-
-//                 // Create and populate video cards
-//                 data.data.forEach(video => {
-//                     const videoCard = document.createElement("div");
-//                     videoCard.classList.add("video-card");
-//                     videoCard.innerHTML = `
-//                         <img src="${video.thumbnail}" alt="${video.title}">
-//                         <h3>${video.title}</h3>
-//                         <p>Views: ${video.others.views}</p>
-//                         <p>Posted Date: ${video.others.posted_date}</p>
-//                         <p>Author: ${video.authors[0].profile_name}</p>
-//                     `;
-//                     videosContainer.appendChild(videoCard);
-//                 });
-//             })
-//             .catch(error => {
-//                 console.error("Error fetching videos:", error);
-//             });
-//     }
-// });
-
-
-
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Fetch the categories data
+
     fetch("https://openapi.programming-hero.com/api/videos/categories")
         .then(response => response.json())
         .then(data => {
             const categoriesContainer = document.querySelector(".categories");
             const videosContainer = document.querySelector(".videos");
 
-            // Create and populate the category buttons
+
             data.data.forEach(category => {
                 const categoryButton = document.createElement("button");
                 categoryButton.textContent = category.category;
@@ -69,11 +18,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 categoriesContainer.appendChild(categoryButton);
             });
 
-            // Load the data for the first category by default
             if (data.data.length > 0) {
                 fetchVideosByCategory(data.data[0].category_id);
             } else {
-                // Handle the case when there are no categories
                 videosContainer.innerHTML = '<img src="./Logo.png" alt="No Data">';
             }
         })
@@ -81,28 +28,33 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("Error fetching categories:", error);
         });
 
-    // Function to fetch and display videos by category
     function fetchVideosByCategory(categoryId) {
         fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
             .then(response => response.json())
             .then(data => {
                 const videosContainer = document.querySelector(".videos");
-                videosContainer.innerHTML = ""; // Clear existing video cards
+                videosContainer.innerHTML = "";
 
                 if (data.data.length === 0) {
-                    // Handle the case when there is no data for the category
                     videosContainer.innerHTML = '<img src="./Icon.png" alt="No Data">';
                 } else {
-                    // Create and populate video cards
                     data.data.forEach(video => {
                         const videoCard = document.createElement("div");
+                        const author = video.authors[0];
+                        const authorName = author.profile_name;
+                        const isVerified = author.verified;
                         videoCard.classList.add("video-card");
+                        const authorHTML = `
+    <p class="authorName">${authorName} ${isVerified ? '<img src="./verifide.png" alt="Verified" id="verifiedIcon">' : ''}</p>
+`;
                         videoCard.innerHTML = `
                             <img src="${video.thumbnail}" alt="${video.title}">
+                            <div class="ProfileNameImage">
+                            <img src="${video.authors[0].profile_picture}">
                             <h3>${video.title}</h3>
-                            <p>Views: ${video.others.views}</p>
-                            <p>Posted Date: ${video.others.posted_date}</p>
-                            <p>Author: ${video.authors[0].profile_name}</p>
+                            </div>
+                            ${authorHTML}
+                            <p class="autheorView">${video.others.views} Views</p>
                         `;
                         videosContainer.appendChild(videoCard);
                     });
